4c18e8fe70869d4aa38b69b4bd02dda32b2659cb
kalindir-lunar <kalindir.lunar@gmail.com>
2024-08-11 17:15:16 +0300

    add: draw bird

--- Program.cs
+++ Program.cs
@@ -9,35 +9,53 @@ namespace HoverbirdConsole;
 
 class Program
 {
-    private static bool _initGame = false; //сделал для начала отрисовки
-    private static bool _gameRun = false; //выполняет, если игра отрисовалась и запущена
+    private static bool _initGame = true;                   //сделал для начала отрисовки
+    private static bool _gameRun = false;                   //выполняет, если игра отрисовалась и запущена
     private static string _starBrick = "*";
     private static bool _birdDirection;
     private static ConsoleKey _lastInputKey;
+    private static bool _spacebarPressed = false;
     private static int _gameScore = 0;
     static void Main(string[] args)
     {
-        if (_initGame == false)
+        if (_initGame == true)
         {
-            DrawGameField();
-            _initGame = true; //Сделал initGame, чтоб один раз поле отрисовывать
-            _gameRun = true;
+            Console.CursorVisible = false;                  //Убрал мигание строки
+            DrawGameField();                                //Отрисовал поле
+            DrawBird(0);                                    //Собственно, сам mister bird
+            _initGame = false;                              //Сделал initGame, чтоб один раз поле отрисовывать
+            _gameRun = true;                                //Запустил основную логику, отрисовку птицы, облаков, движение и тд
         }
 
         while (_gameRun == true)
         {
-            InputController(); //тут пока игра запущена, то управлять игрой, логично?
-            BirdFlyingOnGameField();
-            DrawScore();
-            QuitGame();
+            InputController();                              //Тут пока игра запущена, то идет постоянное считывание нажатой кнопки
+            //DrawScore();
         }
+    }
+
+    private static void InputController()
+    {
+        if (Console.KeyAvailable)
+        {
+            _lastInputKey = Console.ReadKey(true).Key;
+
+            if(_lastInputKey == ConsoleKey.Spacebar)
+            {
+                _spacebarPressed = true;   
+            }
+
+            Thread.Sleep(100);
+        }
+
+        //ChangeBirdDirection();                            //Меняет направление движения птицы после очередного нажатого SpaceBar
+        QuitGame();                                         //Остановка программы при нажатии Q
     }
 
     private static void DrawGameField()
     {
         Console.Clear();
-        Console.BackgroundColor = ConsoleColor.Blue;
-        //Console.SetBackgroundColor(50, 85, 120, 255); //сделал голубое небо
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
 
         for (int i = 0; i < 40; i++)
         {
@@ -58,48 +76,59 @@ class Program
                 Console.WriteLine(_starBrick + _starBrick.PadLeft(60));
         }
         Console.ResetColor();
-        //Отрисовал поле и вернул цвет, чтоб следующие
     }
 
     private static void DrawScore()
     {
-        Thread.Sleep(500);
+        Thread.Sleep(10);
         _gameScore++;
-        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
         Console.SetCursorPosition(0, 5);
         Console.Write(_starBrick + _gameScore.ToString().PadLeft(30) + _starBrick.PadLeft(30));
         Console.ResetColor();
     }
 
-    private static void InputController()
+    private static void DrawBird(int birdXCoordinate)
     {
-        if (Console.KeyAvailable)
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        int[] yPositions = {31, 32, 33};
+
+        string[] lines = {  //Mister Bird
+                "A",        //lines.Length 0
+              "/^O^\\",     //lines.Length 1
+                "X"         //lines.Length 2
+        };
+
+        for (int i = 0; i < lines.Length; i++)
         {
-            var key = Console.ReadKey(true).Key;
-            _lastInputKey = key;
-            Task.Delay(100);
+            Console.SetCursorPosition(0, yPositions[i]);
+            Console.Write(_starBrick);
+            if(i == 1)
+            {
+                Console.Write(" ".PadLeft(30 + birdXCoordinate - lines[i].Length+2));
+            }
+            else
+                Console.Write(" ".PadLeft(30 + birdXCoordinate - lines[i].Length));
+    
+            foreach (char c in lines[i])
+            {
+                Console.Write(c);
+            }
         }
+
+        Console.ResetColor();
     }
 
-    private static void BirdFlyingOnGameField()
+    private static void ChangeBirdDirection()
     {
-        if (_lastInputKey == ConsoleKey.Spacebar)
+        if (_spacebarPressed == true)
         {
             _birdDirection = !_birdDirection;
             ClearLineInConsole(0,41);
             Console.SetCursorPosition(0, 41);
             Console.WriteLine("Direction: " + _birdDirection);
+            _spacebarPressed = false;
         }
-
-        //ЭТО ПОТОМ УДАЛЮ, ОНО НУЖНА ДЛЯ ДЕБАГА!!!
-        if (_lastInputKey != ConsoleKey.Spacebar &&
-            _lastInputKey != ConsoleKey.Q)
-        {
-            ClearLineInConsole(0,41);
-            Console.SetCursorPosition(0, 41);
-            Console.WriteLine("Key:" + _lastInputKey.ToString());
-        }
-        //ТУТ КОНЕЦ ДЕБАГА!!!
     }
 
     private static void QuitGame()
@@ -107,11 +136,13 @@ class Program
         if (_lastInputKey == ConsoleKey.Q)
         {
             _gameRun = false;
-            ClearLineInConsole(0,41);
+            //ClearLineInConsole(0,41);
             Console.SetCursorPosition(0, 41);
             Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Quit from the game. Good luck!");
             Console.ResetColor();
+            Thread.Sleep(1000);
+            Console.Clear();
         }
     }
 
